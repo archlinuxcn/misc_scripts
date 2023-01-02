dacdd6a13fd089707ece89a09f64b4b0f7d9614b
lilydjwg <lilydjwg@gmail.com>
2023-01-02 21:17:38 +0800

    mirror-delay: xtom has multiple mirrors with the same stem host

--- mirror-delay/src/util.rs
+++ mirror-delay/src/util.rs
@@ -1,11 +1,18 @@
-pub fn name_from_url(url: &url::Url) -> &str {
+use std::borrow::Cow;
+
+pub fn name_from_url(url: &url::Url) -> Cow<str> {
   if let Some(url::Host::Domain(host)) = url.host() {
     if let Some(d) = psl::domain(host.as_bytes()) {
       let whole = d.as_bytes();
       let suffix = d.suffix().as_bytes();
       let main = &whole[..suffix.as_ptr() as usize - whole.as_ptr() as usize];
-      return std::str::from_utf8(main).unwrap().trim_end_matches('.');
+      let host_stem = std::str::from_utf8(main).unwrap().trim_end_matches('.');
+      return if host_stem == "xtom" {
+        Cow::Owned(std::str::from_utf8(whole).unwrap().replace('.', "_"))
+      } else {
+        Cow::Borrowed(host_stem)
+      };
     }
   }
-  "host_invalid"
+  Cow::Borrowed("host_invalid")
 }
