75801cac3203f2b5be08e34818a8d17ed35027e1
lilydjwg <lilydjwg@gmail.com>
2025-05-16 15:56:40 +0800

    matrixbot: support reply & edit

--- matrixbot/src/ipc.rs
+++ matrixbot/src/ipc.rs
@@ -2,8 +2,17 @@ use matrix_sdk::{Client, Room};
 use matrix_sdk::ruma::{
   self,
   OwnedRoomAliasId, OwnedRoomOrAliasId,
-  OwnedUserId, OwnedRoomId,
-  events::room::message::RoomMessageEventContent,
+  OwnedUserId, OwnedRoomId, OwnedEventId, EventId,
+  events::{
+    AnySyncTimelineEvent,
+    AnySyncMessageLikeEvent,
+    SyncMessageLikeEvent,
+    room::message::{
+      RoomMessageEventContent, ReplacementMetadata,
+      ForwardThread, AddMentions,
+      OriginalRoomMessageEvent,
+    },
+  },
 };
 use matrix_sdk::deserialized_responses::TimelineEvent;
 use tokio::net::UnixDatagram;
@@ -21,7 +30,13 @@ struct Ipc {
 #[serde(tag="cmd")]
 enum Message {
   #[serde(rename="send_message")]
-  SendMessage { target: OwnedRoomOrAliasId, content: String, html_content: Option<String> },
+  SendMessage {
+    target: OwnedRoomOrAliasId,
+    content: String,
+    html_content: Option<String>,
+    reply_to: Option<OwnedEventId>,
+    replaces: Option<OwnedEventId>,
+  },
   #[serde(rename="delete_user_messages")]
   DeleteUserMessages { target: OwnedRoomOrAliasId, user: OwnedUserId },
 }
@@ -41,14 +56,27 @@ async fn handle_msg(ipc: &Ipc, msg: &[u8]) -> Result<()> {
   event!(Level::DEBUG, msg = msg_str, "got message");
   let msg = serde_json::from_str(msg_str)?;
   match msg {
-    Message::SendMessage { target, content, html_content } => {
+    Message::SendMessage { target, content, html_content, reply_to, replaces } => {
       event!(Level::INFO, msg = msg_str, "sending message");
       let room_id = resolve_to_room_id(&ipc.client, target).await?;
       if let Some(room) = ipc.client.get_room(&room_id) {
-        let msg = match html_content {
+        let mut msg = match html_content {
           Some(html) => RoomMessageEventContent::text_html(content, html),
           None => RoomMessageEventContent::text_plain(content),
         };
+        let replied_msg = if let Some(evt) = reply_to {
+          room_message_event(&room, &evt).await
+        } else {
+          None
+        };
+        if let Some(evt) = replaces {
+          let meta = ReplacementMetadata::new(evt, None);
+          msg = msg.make_replacement(meta, replied_msg.as_ref());
+        } else if let Some(replied_msg) = replied_msg {
+          msg = msg.make_reply_to(
+            &replied_msg, ForwardThread::Yes, AddMentions::Yes,
+          );
+        }
         room.send(msg).await?;
       } else {
         event!(Level::ERROR, msg = msg_str, "room not found");
@@ -145,4 +173,18 @@ pub fn enable(client: Client, mut path: String) -> Result<()> {
   let ipc = Ipc { socket, client };
   tokio::spawn(ipc_task(ipc));
   Ok(())
+}
+
+async fn room_message_event(
+  room: &Room, event_id: &EventId,
+) -> Option<OriginalRoomMessageEvent> {
+  let m = room.event(event_id, None).await.ok()?;
+  let timeline_event = m.into_raw().deserialize().ok()?;
+  let AnySyncTimelineEvent::MessageLike(message_like) =
+    timeline_event else { return None };
+  let AnySyncMessageLikeEvent::RoomMessage(rm) =
+    message_like else { return None };
+  let SyncMessageLikeEvent::Original(orignal) =
+    rm else { return None };
+  Some(orignal.into_full_event(room.room_id().to_owned()))
 }
